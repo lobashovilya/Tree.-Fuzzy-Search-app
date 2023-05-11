3b27f53972f93ccaf5d896a449262fcca469381b
lobashovilya <lobashov1999@mail.ru>
2023-05-11 11:58:32 +0300

    Tree. Fuzzy Search app

---
+++ Кур/Кур/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Кур
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Искать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(14, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите слово";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(7, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 14);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Вы имели ввиду";
+            this.label2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox3.Location = new System.Drawing.Point(334, 51);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(356, 175);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(476, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 21);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Словарь";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(102, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Visible = false;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 257);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Кур/Кур/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Кур
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            using (StreamReader stream = new StreamReader("slovar.txt"))
+            {
+                while (stream.Peek() >= 0)
+                {
+                    string str = stream.ReadLine();
+                    string[] tmp = str.Split(',');
+                    slovar = tmp;
+                }
+            }
+            foreach (string word in slovar)
+            {
+                textBox3.Text += word + ", ";
+            }
+            textBox3.Text = textBox3.Text.Remove(textBox3.Text.Length - 1);
+            textBox3.Text = textBox3.Text.Remove(textBox3.Text.Length - 1);
+            textBox3.Text += ".";
+        }
+        string[] slovar;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class Tree
+        {
+            public class TreeNode
+            {
+                private readonly Dictionary<char, TreeNode> subNode; // подузел
+                private bool isEnd; // конец слова
+                private readonly string word; // слово
+                public TreeNode(string entering_word)
+                {
+                    subNode = new Dictionary<char, TreeNode>();
+                    isEnd = false;
+                    word = entering_word;
+                }
+                public Dictionary<char, TreeNode> SubNode { get { return subNode; } }
+                public bool IsEnd { get { return isEnd; } set { isEnd = value; } }
+                public string Word { get { return word; } }
+            }
+
+                public TreeNode root;
+                public Tree()
+                {
+                    root = new TreeNode(String.Empty);
+                }
+                public void Add(string word)
+                {
+                    AddRecursive(root, word, String.Empty);
+                }
+                public static void AddRecursive(TreeNode node, string entering_string, string current_string)
+                {
+                    if (entering_string.Length == 0) { return; }
+                    char prefix = entering_string[0];
+                    string substring = entering_string.Substring(1);
+                    if (!node.SubNode.ContainsKey(prefix))
+                    {
+                        node.SubNode.Add(prefix, new TreeNode(current_string + prefix));
+                    }
+                    if (substring.Length == 0)
+                    {
+                        node.SubNode[prefix].IsEnd = true;
+                        return;
+                    }
+                    else
+                    {
+                        AddRecursive(node.SubNode[prefix], substring, current_string + prefix);
+                    }
+                }
+                public IEnumerable<string> Find (string find_word)
+                {
+                    TreeNode node = root;
+                    foreach (var find in find_word)
+                    {
+                        if (!node.SubNode.ContainsKey(find))
+                        {
+                            return new string[0];
+                        }
+                        node = node.SubNode[find];
+                    }
+                    return Checkword(node);
+                }
+                public IEnumerable<string> Checkword (TreeNode node)
+                {
+                    if (node.IsEnd)
+                    {
+                        yield return node.Word;
+                    }
+
+                foreach (var subnode in node.SubNode)
+                {
+                    foreach (var result in Checkword(subnode.Value))
+                    {
+                        yield return result;
+                    }
+                }
+                }
+            public IEnumerable<string> Obhod()
+            {
+                TreeNode node = root;
+                return Checkword(node);
+            }  
+
+        }
+        public static int rasdamlen(string first, string second)
+        {
+            if (string.IsNullOrEmpty(first))
+                if (string.IsNullOrEmpty(second))
+                    return 0;
+                else
+                    return second.Length;
+            else if (string.IsNullOrEmpty(second))
+                return first.Length;
+
+            if (second.Length > first.Length)
+            {
+                string tmp = second;
+                second = first;
+                first = tmp;
+            }
+
+            int[] firstDp = new int[second.Length + 1];
+            int[] secondDp = new int[second.Length + 1];
+            int[] thirdDp = new int[second.Length + 1];
+            int inf = first.Length + second.Length;
+
+            for (int j = 0; j <= second.Length; ++j)
+                thirdDp[j] = j;
+
+            for (int i = 1; i <= first.Length; ++i)
+            {
+                int[] tmp = firstDp;
+                firstDp = secondDp;
+                secondDp = thirdDp;
+                thirdDp = tmp;
+                thirdDp[0] = secondDp[0] + 1;
+                for (int j = 1; j <= second.Length; ++j)
+                {
+                    int[] dist = new int[] {
+                        secondDp[j] + 1,
+                        thirdDp[j - 1] + 1,
+                        secondDp[j - 1] + (first[i - 1] != second[j - 1] ? 1 : 0),
+                        i > 1 && j > 1 ? firstDp[j - 2] + (first[i - 1] == second[j - 2] && first[i - 2] == second[j - 1] ? 1 : inf) : inf
+                    };
+                    thirdDp[j] = dist.Min();
+                }
+            }
+            return thirdDp[second.Length];
+        }
+
+        public double pohoj (string etalon, string srav)
+        {
+            int k = etalon.Length + srav.Length;
+            int c = 0;
+            for (int i = 0; i < etalon.Length; i++)
+                for (int j = 0; j < srav.Length; j++)
+                {
+                    if (etalon[i] == srav[j])
+                    {
+                        srav = srav.Remove(j, 1);
+                        srav = srav.Insert(j, " ");
+                        c++;
+                    }
+                }
+            double x;
+            return x = (double)c / (double)(k - c);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            textBox2.Visible = false;
+            comboBox1.Items.Clear();
+            comboBox1.Visible = false;
+            Queue<string> queue = new Queue<string>();
+            Queue<string> queue1 = new Queue<string>();
+            textBox2.Clear();
+            if (textBox1.Text == String.Empty)
+            { MessageBox.Show("Вы ничего не ввели!"); return; }
+            string search_word = textBox1.Text.ToLower();
+            bool flag = false;
+            foreach(char simb in search_word)
+            {
+                if ((simb >= 'а') && (simb <= 'я'))
+                { flag = true; }
+            }
+            if (flag)
+            { MessageBox.Show("Можно вводить только латинские буквы!"); textBox1.Clear();    return; }
+            try
+            {
+                int stroka = Convert.ToInt32(search_word);
+                MessageBox.Show("Вы ввели не слово!");
+                textBox1.Clear();
+                return;
+            }
+            catch {}
+            textBox1.Text = textBox1.Text.ToLower();
+            Tree trie = new Tree();
+            foreach (string word in slovar)
+            {
+                trie.Add(word);
+            }
+            var derevo = trie.Obhod();
+            var result = trie.Find(search_word);
+            if (result.Count() == 0)
+            {
+                foreach (string word in derevo)
+                {
+                    if (rasdamlen(word, search_word) < 4)
+                    { queue.Enqueue(word); }
+                }
+            }
+            else
+            {
+                foreach (var word in result)
+                {
+                    if (rasdamlen(word, search_word) < 4)
+                    { queue.Enqueue(word); }
+                }
+            }
+            if (queue.Count == 0)
+            { MessageBox.Show("Вы сделали слишком много ошибок!"); textBox1.Clear(); return; }
+            double x = 0;
+            while (queue.Count != 0)
+            {
+               double y = pohoj(search_word, queue.Peek());
+                if (x<y)
+               {
+                    queue1.Clear();
+                    x = y;
+                    queue1.Enqueue(queue.Peek());
+               }
+                else if (x == y)
+                { queue1.Enqueue(queue.Peek()); }
+                   queue.Dequeue();
+            }
+            if (queue1.Count > 1)
+            {
+                label2.Visible = true;
+                comboBox1.Visible = true;
+                foreach (var word in queue1)
+                {
+                    comboBox1.Items.Add(word);
+                }
+            }
+            else
+            {
+                textBox2.Visible = true;
+                textBox2.Text = queue1.Peek();
+            }
+        }
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            textBox2.Visible = true;
+            textBox2.Text = Convert.ToString(comboBox1.SelectedItem);
+            comboBox1.Visible = false;
+        }
+    }
+ }
